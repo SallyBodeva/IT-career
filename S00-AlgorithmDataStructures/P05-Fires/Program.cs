7eae095d581dd7b203fc8820958d4fdc10f1bb91
salihe <salihe.bodeva10@gmail.com>
2023-09-24 23:15:48 +0300

    ADS

---
+++ S00-AlgorithmDataStructures/P05-Fires/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] dimensions = Console.ReadLine().Split();
+        int n = int.Parse(dimensions[0]);
+        int m = int.Parse(dimensions[1]);
+
+        int[][] matrix = new int[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i] = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        }
+
+        int fireCount = CountFires(matrix, n, m);
+        Console.WriteLine(fireCount);
+    }
+
+    static int CountFires(int[][] matrix, int n, int m)
+    {
+        int count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i][j] == 1)
+                {
+                    // Намерен е нов пожар, увеличаваме брояча и извикваме функцията за намиране на огнищата
+                    count++;
+                    MarkFire(matrix, n, m, i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    static void MarkFire(int[][] matrix, int n, int m, int i, int j)
+    {
+        if (i < 0 || i >= n || j < 0 || j >= m || matrix[i][j] == 0)
+        {
+            return;
+        }
+
+        // Маркираме текущата клетка като посетена (0)
+        matrix[i][j] = 0;
+
+        // Проверяваме съседните клетки
+        MarkFire(matrix, n, m, i - 1, j - 1);
+        MarkFire(matrix, n, m, i - 1, j);
+        MarkFire(matrix, n, m, i - 1, j + 1);
+        MarkFire(matrix, n, m, i, j - 1);
+        MarkFire(matrix, n, m, i, j + 1);
+        MarkFire(matrix, n, m, i + 1, j - 1);
+        MarkFire(matrix, n, m, i + 1, j);
+        MarkFire(matrix, n, m, i + 1, j + 1);
+    }
+}
---
+++ S00-AlgorithmDataStructures/P06-Recursion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P06_Recursion
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            PrintNums(10);
+        }
+        public static void PrintNums(int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+            PrintNums(n - 1);
+            Console.Write(n+" ");
+        }
+    }
+}
