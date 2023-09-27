ab4133927ee3680d32410904359efa87c4f18cbb
salihe <salihe.bodeva10@gmail.com>
2023-09-27 22:59:50 +0300

    ADS

---
+++ S00-AlgorithmDataStructures/P07-Connections/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07_Connections
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string command = string.Empty;
+            List<string> lakes = new List<string>();
+            int connectedCount = 0;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                lakes.Add(command);
+            }
+            for (int i = 0; i < lakes.Count; i++)
+            {
+                string currentLake = lakes[i];
+                for (int j = 0; j < lakes.Count; j++)
+                {
+                    string secondCurrentLake = lakes[j];
+                    if (currentLake == new string(secondCurrentLake.Reverse().ToArray()))
+                    {
+                        char[] letters = currentLake.ToCharArray();
+                        Console.WriteLine($"{letters[0]}<->{letters[1]}");
+                        lakes.Remove(secondCurrentLake);
+                        connectedCount++;
+                        break;
+                    }
+                   
+                }
+            }
+            if (connectedCount==0)
+            {
+                Console.WriteLine("Disconnected...");
+            }
+        }
+    }
+}
---
+++ S00-AlgorithmDataStructures/P08-Chemistry/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, List<string>> possibleReactions = new Dictionary<string, List<string>>();
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "end")
+                break;
+
+            string[] elements = input.Split();
+            string parentElement = elements[0];
+            string childElement = elements[1];
+
+            if (!possibleReactions.ContainsKey(parentElement))
+                possibleReactions[parentElement] = new List<string>();
+
+            possibleReactions[parentElement].Add(childElement);
+        }
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "end")
+                break;
+
+            string[] query = input.Split('-');
+            string parentElement = query[0].Trim();
+            string[] childElements = query[1].Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            bool isValid = true;
+            foreach (var childElement in childElements)
+            {
+                if (!possibleReactions.ContainsKey(parentElement) || !possibleReactions[parentElement].Contains(childElement))
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine($"{parentElement} - {string.Join(", ", childElements)}");
+            }
+        }
+    }
+}
